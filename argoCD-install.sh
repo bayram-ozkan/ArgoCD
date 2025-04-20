629c7f45aaf9872314a5111419324d7307fe9c5e
nakzo <bayram_0310@hotmail.com>
2025-04-20 17:19:36 +0300

    begin

---
+++ argoCD-install.sh
@@ -0,0 +1,17 @@
+
+#kubectl apply -f application.yaml
+
+
+
+kubectl create namespace argocd
+
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.1.3/manifests/install.yaml
+
+
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+    
+
+
+kubectl -n argocd port-forward svc/argocd-server -n argocd 8081:443
