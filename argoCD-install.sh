9a3197a3e90c808beb082135a9b71b7a269a40ce
nakzo <bayram_0310@hotmail.com>
2025-04-21 19:42:14 +0300

    Kurulum adımları

--- argoCD-install.sh
+++ argoCD-install.sh
@@ -1,11 +1,8 @@
 
-#kubectl apply -f application.yaml
-
-
-
 kubectl create namespace argocd
 
 
+# https://argo-cd.readthedocs.io/en/stable/getting_started/
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.1.3/manifests/install.yaml
 
 
@@ -13,5 +10,18 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
     
 
+kubectl port-forward svc/argocd-server -n argocd 8080:443
 
-kubectl -n argocd port-forward svc/argocd-server -n argocd 8081:443
+
+# http://localhost:8080
+
+
+# default   username   ```admin```    ve   şifre   
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+
+
+# Test uygulaması 
+
+kubectl apply -f application.yaml
